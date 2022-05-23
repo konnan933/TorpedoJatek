456c784e6f9a8e12b73ceceabb7004b643cb423a
konnan933 <konnan933@gmail.com>
2022-05-23 14:49:40 +0200

    class Teszt és teszlövés metódus

---
+++ src/main/TorpedoTeszt.java
@@ -0,0 +1,14 @@
+
+package main;
+
+public class TorpedoTeszt {
+    
+    
+    public static void main(String[] args) {
+        lovesteszt(4);
+    }
+
+    public static String lovesteszt(int poz) {
+        return "Teszt";
+    }
+}
