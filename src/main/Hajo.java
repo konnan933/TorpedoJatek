4e7bacde0632ca617f469bccb3765102482b5bd3
konnan933 <konnan933@gmail.com>
2022-05-23 15:37:29 +0200

    adattagok lovesSzam talalatSzam és lovesBe, járék metódus muködő képes verzió

--- src/main/Hajo.java
+++ src/main/Hajo.java
@@ -1,22 +1,39 @@
 
 package main;
 
+import java.util.Scanner;
+
 public class Hajo {
     private int[] poz = new int[3];
+    private int[] palya = new int[7];
+    private int talalatSzam = 0;
+    private int lovesSzam = 0;
 
     public Hajo() {
         setPoz(poz);
     }
-
+    public void jatek(){
+        while (talalatSzam < 3) {            
+            lovesBe();
+        }
+        System.out.println("Elsüllyedt ennyi lövésből:"+lovesSzam);
+        
+    }
     public void setPoz(int[] poz) {
-        int kezdoHely = 3;//(int)Math.random()*5;
+        int kezdoHely = (int) (Math.random() *(palya.length-poz.length+1));
         for (int i = 0; i < poz.length; i++) {
             this.poz[i] = kezdoHely;
             kezdoHely++;
         }
         
     }
-    
+    public void lovesBe(){
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Kérem adja emg hova szeretne lóni(1-7)");
+        int poz = scr.nextInt()-1;
+        talalat(poz);
+        
+    }
     
     
     public String talalat(int poz){
@@ -24,6 +41,13 @@ public class Hajo {
         while (i < this.poz.length && !(poz == this.poz[i])) {            
             i++;
         }
+        if (i < this.poz.length) {
+            System.out.println("Találat");
+            talalatSzam++;
+        }else{
+            System.out.println("Nem talált");
+        }
+        lovesSzam++;
         return i < this.poz.length? "találat": "nem talált";
     }
     
--- src/main/Program.java
+++ src/main/Program.java
@@ -5,7 +5,9 @@ package main;
 public class Program {
 
     public static void main(String[] args) {
-
+        System.out.println("Torpedo játék:");
+        Hajo hajo = new Hajo();
+        hajo.jatek();
     }
     
 }
